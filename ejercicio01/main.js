f568005b08af6490fbd34b3aff146b497b36b483
iglesias-juan <iglesias.juan@outlook.es>
2024-10-11 12:05:40 +0200

    Feat: commit 2

---
+++ ejercicio01/main.js
@@ -0,0 +1,29 @@
+//! 1.1
+
+const button = document.querySelector(`.showme`)
+console.log(button)
+
+//! 1.2
+
+const h1 = document.querySelector(`#pillado`)
+console.log(h1)
+
+//! 1.3
+
+const p = document.querySelectorAll(`p`)
+console.log(p)
+
+//! 1.4
+
+const h4 = document.querySelectorAll(`.pokemon`)
+console.log(h4)
+
+//! 1.5
+
+// const span = document.querySelectorAll(`[data-function="testMe"]`)
+// console.log(span)
+
+//! 1.6
+
+const span = document.querySelectorAll(`[data-function="testMe"]`)
+console.log(span[2])
