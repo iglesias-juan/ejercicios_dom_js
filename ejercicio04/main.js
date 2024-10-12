85cc68e5431745f31d9dcf1ce171cafeb1dfe0bc
iglesias-juan <iglesias.juan@outlook.es>
2024-10-12 14:34:15 +0200

    Feat: commit 5

---
+++ ejercicio04/main.js
@@ -0,0 +1,13 @@
+//! 4.1
+
+document.addEventListener(`DOMContentLoaded`, () => {
+  const button = document.getElementById(`btnToClick`)
+
+  button.addEventListener(`click`, (Event) => {
+    console.log(`Esto es JavaScript!🤯`, Event)
+  })
+})
+
+//! 4.2
+//! 4.3
+//! Bonus track!
