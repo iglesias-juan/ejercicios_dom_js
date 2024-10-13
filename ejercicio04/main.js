7a85ff8de07b0b2bd955d7a8af2a73bc4d698ffd
iglesias-juan <iglesias.juan@outlook.es>
2024-10-13 17:18:24 +0200

    Feat: commit 6

--- ejercicio04/main.js
+++ ejercicio04/main.js
@@ -9,5 +9,44 @@ document.addEventListener(`DOMContentLoaded`, () => {
 })
 
 //! 4.2
+
+const focus = document.querySelector(`.focus`)
+
+focus.addEventListener(`focus`, function () {
+  console.log(focus.value)
+})
+
 //! 4.3
+
+const valueInput = document.querySelector(`.value`)
+
+valueInput.addEventListener(`value`, function () {
+  console.log(valueInput.value)
+})
+
+valueInput.addEventListener(`input`, function () {
+  console.log(valueInput.value)
+})
+
 //! Bonus track!
+
+const albums = [
+  'De Mysteriis Dom Sathanas',
+  'Reign of Blood',
+  'Ride the Lightning',
+  'Painkiller',
+  'Iron Fist'
+]
+
+const body = document.body
+
+const ul = document.createElement(`ul`)
+
+albums.forEach((album) => {
+  const li = document.createElement(`li`)
+  li.textContent = album
+
+  ul.appendChild(li)
+})
+
+body.appendChild(ul)
